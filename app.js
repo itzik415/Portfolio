4d76577a09927e1ea74b8882642cf7b4775826ec
Itzik Shaoulian <itzikshaoulian@gmail.com>
2018-09-16 19:04:13 -0400

    fixing server bug

--- app.js
+++ app.js
@@ -73,5 +73,5 @@ app.post('/send', (req, res) => {
   });
   });
 
-var port_number = server.listen(process.env.PORT || 3000);
+var port_number = app.listen(process.env.PORT || 3000);
 app.listen(port_number);
