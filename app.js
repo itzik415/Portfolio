c5b2ac3503aad317358a9cc9566ae7afaef3c223
Itzik Shaoulian <itzikshaoulian@gmail.com>
2018-09-16 18:18:22 -0400

    change the get request

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.render('index.html');
+  res.render('index');
 });
 
 app.post('/send', (req, res) => {
